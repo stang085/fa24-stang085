e7291ef1d2dd9f01153dc74a70c7d677f2bfa7d3
Sydney Tang <stang085@berkeley.edu>
2024-09-23 19:36:17 +0000

    changes

--- app.rb
+++ app.rb
@@ -40,6 +40,19 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    
+    begin
+      if letter.nil? || letter.empty? || letter.length > 1 || !letter.match?(/[a-zA-Z]/)
+        flash[:message] = "Invalid guess."
+      elsif @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
+        flash[:message] = "You have already used that letter."
+      else
+        @game.guess(letter)
+      end
+    rescue ArgumentError => e
+      flash[:message] = "Invalid guess."
+    end
+
     redirect '/show'
   end
   
@@ -50,17 +63,36 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    # Check for win or lose conditions
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win
+    else
+      redirect '/show'
+    end
+    #erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose
+    else
+      redirect '/show'
+    end
+    #erb :lose # You may change/remove this line
   end
   
 end
